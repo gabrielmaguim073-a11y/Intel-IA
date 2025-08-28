859a877b109acfdec98b3bf03916bffaf69334e1
gabrielmaguim073-a11y <gabrielmaguim073@gmail.com>
2025-08-28 00:19:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,5 +7,4 @@ def home():
     return "Olá, Mundo! 🚀 Sua IA já está rodando no Render!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render define a porta em PORT
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=10000)
