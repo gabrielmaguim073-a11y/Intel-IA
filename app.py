ea566b4cbb3a3a8d958784ccd2491c36e42297d3
gabrielmaguim073-a11y <gabrielmaguim073@gmail.com>
2025-08-28 00:07:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def home():
     return "Olá, Mundo! 🚀 Sua IA já está rodando no Render!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Pega a porta do Render
+    port = int(os.environ.get("PORT", 5000))  # Render define a porta em PORT
     app.run(host="0.0.0.0", port=port)
