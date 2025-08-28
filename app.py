8b1602f2b959e24f66343a590c4ab8bb434583ca
gabrielmaguim073-a11y <gabrielmaguim073@gmail.com>
2025-08-27 22:06:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🚀 Sua IA está rodando com Flask!"
+    return "🚀 Sua IA está rodando no Heroku com sucesso!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
