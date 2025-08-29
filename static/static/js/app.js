b6a99060db25a5f9315da20f78378c48e270d383
gabrielmaguim073-a11y <gabrielmaguim073@gmail.com>
2025-08-29 14:47:25 -0300

    Update app.js

--- static/static/js/app.js
+++ static/static/js/app.js
@@ -1,1 +1,12 @@
+// Arquivo: static/js/app.js
 
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("🚀 App carregado com sucesso!");
+
+    const titulo = document.querySelector("h1");
+    if (titulo) {
+        titulo.addEventListener("click", () => {
+            alert("Você clicou no título!");
+        });
+    }
+});
