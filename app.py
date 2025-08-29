062c51bdaa8dcf61818c93cf2e0f2bdfab4f393a
gabrielmaguim073-a11y <gabrielmaguim073@gmail.com>
2025-08-28 22:02:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,6 @@
-import os
 from flask import Flask
-
-# pega a chave salva no Render
-API_KEY = os.getenv("API_KEY")
-if not API_KEY:
-    raise RuntimeError("API_KEY não está definida no ambiente")
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return f"Sua API_KEY está carregada e o servidor Flask está rodando! Chave: {API_KEY[:5]}***"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return "IA Online!"
